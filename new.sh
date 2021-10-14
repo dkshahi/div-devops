21bb7a137b1cb06b20d489ef61401f66521793d7
D Shahi <dShahi@gmail.com>
2021-10-14 08:01:22 +0530

    update the file in dev

--- new.sh
+++ new.sh
@@ -1,1 +1,12 @@
 echo "This is my first file for shell script"
+
+this is my first file
+this is second line of cod
+this is third line
+
+how is training going on
+
+I am pdating file to create a conflict in git
+echo "Hello Team"
+
+
